96bbb20bf3ed961048a1c6be9cf6b7344f69f9ae
jonniebigodes <joaocontadesenvolvimento@gmail.com>
2026-06-14 16:24:43 +0100

    Reverts headless and browsers

--- vite.config.ts
+++ vite.config.ts
@@ -58,7 +58,7 @@ export default defineConfig({
           setupFiles: ["./src/test-setup.ts"],
           browser: {
             enabled: true,
-            headless: false,
+            headless: true,
             screenshotFailures: false,
             provider: playwright({}),
             viewport: {
@@ -69,13 +69,13 @@ export default defineConfig({
               {
                 browser: "chromium",
               },
-              /* {
+              {
                 browser: "firefox",
               },
 
               {
                 browser: "webkit",
-              }, */
+              },
             ],
           },
         },
